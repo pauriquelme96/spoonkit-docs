6ca644fe5ab3bf4c10b1144bdc6ce7f68c6d6650
Pau Riquelme Catalina <pau.riquelme96@gmail.com>
2025-10-30 17:41:37 +0100

    Add missing component prop to Button

--- src/components/Button/ButtonCtrl.ts
+++ src/components/Button/ButtonCtrl.ts
@@ -1,12 +1,13 @@
 import { Ctrl } from "../../lib/Ctrl";
 import { emitter } from "../../lib/signals/Emitter";
 import { state } from "../../lib/signals/State";
+import { Button } from "./Button";
 
 export class ButtonCtrl extends Ctrl {
+  component = Button;
   public label = state<string>("");
   public disabled = state<boolean>(false);
   public variant = state<"primary" | "secondary" | "danger">("primary");
   public loading = state<boolean>(false);
-
   public onClick = emitter<void>();
 }
